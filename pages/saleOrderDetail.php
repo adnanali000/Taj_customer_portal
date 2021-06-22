75ae1e19b415c5400d6dc6045986b6d15bb285ef
adnanali000 <adnan.museel123@gmail.com>
2021-06-22 22:15:32 +0500

    status bar updated

--- pages/saleOrderDetail.php
+++ pages/saleOrderDetail.php
@@ -45,6 +45,34 @@ if( isset($_GET['salesid'])){
     
 </style>
 <body>
+<?php
+
+$query = "select ST.SALESID,PSJ.PACKINGSLIPID,PSJ.DELIVERYDATE AS SHIPDATE,
+ST.SHIPPINGDATEREQUESTED AS SCHEDULEDATE,
+
+ST.CREATEDDATETIME AS SUBMITEDDATE,CPST.ITEMID AS ITEMCODE, 
+CPST.NAME ITEMNAME,upper(CPST.SALESUNIT) AS UoM
+
+,CPST.QTY RCVNGQTY,CJR.INVOICEID,CJR.INVOICEDATE
+
+FROM SALESTABLE ST
+
+INNER JOIN CUSTPACKINGSLIPJOUR PSJ ON ST.SALESID = PSJ.SALESID
+
+INNER JOIN CUSTPACKINGSLIPTRANS CPST ON PSJ.PACKINGSLIPID = CPST.PACKINGSLIPID
+
+INNER JOIN CUSTINVOICEJOUR AS CJR ON CJR.SALESID = ST.SALESID
+
+
+WHERE ST.SALESID = '".$saleid."' ";
+
+$stmt = sqlsrv_query($conn, $query, array(), array("Scrollable"=>'static')) or DIE(sqlsrv_errors());
+
+//   $res = sqlsrv_fetch_array($stmt);
+  
+  while($res = sqlsrv_fetch_array($stmt)){
+?>
+
     <div class="container-fluid">
 
     <!-- summary header  -->
@@ -53,20 +81,20 @@ if( isset($_GET['salesid'])){
     <section class="text-gray-600 mb-3 body-font" id="status">
   <div class="container py-3  flex flex-wrap flex-col">
     <div class="flex mx-auto flex-wrap">
-      <a class="sm:px-6 py-3 w-1/2 sm:w-auto justify-center sm:justify-start border-b-2 title-font font-medium   items-center leading-none  text-indigo-500 tracking-wider rounded-t">
-        <i class="fas fa-check" style="color:green;margin:5px;font-size:22px;" ></i> Submitted <p id="submit">11 Jun 2021</p>
+      <a class="sm:px-6 py-3 w-1/2 sm:w-auto justify-center sm:justify-start border-b-2 title-font font-medium items-center leading-none  text-indigo-500 tracking-wider rounded-t" id="bgsubmit">
+        <i class="fas fa-check" style="color:green;margin:5px;font-size:22px;" id="sub"></i> Submitted <p id="submit"><?php echo $res['SUBMITEDDATE']->format('d-m-Y')?></p>
       </a>
-      <a class="sm:px-6 py-3 w-1/2 sm:w-auto justify-center sm:justify-start border-b-2 title-font font-medium   items-center leading-none  text-indigo-500 tracking-wider rounded-t">
-        <i class="fas fa-check" style="color:green;margin:5px;font-size:22px;" ></i> Scheduled <p id="schedule">11 Jun 2021</p>
+      <a class="sm:px-6 py-3 w-1/2 sm:w-auto justify-center sm:justify-start border-b-2 title-font font-medium   items-center leading-none  text-indigo-500 tracking-wider rounded-t" id="bgschedule">
+        <i class="fas fa-check" style="color:green;margin:5px;font-size:22px;" id="sch"></i> Scheduled <p id="schedule"><?php echo $res['SCHEDULEDATE']->format('d-m-Y')?></p>
       </a>
-      <a class="sm:px-6 py-3 w-1/2 sm:w-auto justify-center sm:justify-start bg-gray-100 border-indigo-500 border-b-2 title-font font-medium   items-center leading-none  text-indigo-500 tracking-wider rounded-t">
-        <i class="fas fa-check" style="color:green;margin:5px;font-size:22px;" ></i> Shipped <p id="ship">11 Jun 2021</p>
+      <a class="sm:px-6 py-3 w-1/2 sm:w-auto justify-center sm:justify-start  border-b-2 title-font font-medium   items-center leading-none  text-indigo-500 tracking-wider rounded-t" id="bgship">
+        <i class="fas fa-check" style="color:green;margin:5px;font-size:22px;" id="shi"></i> Shipped <p id="ship"><?php echo $res['SHIPDATE']->format('d-m-Y');?></p>
       </a>
-      <a class="sm:px-6 py-3 w-1/2 sm:w-auto justify-center sm:justify-start border-b-2 title-font font-medium   items-center leading-none  text-indigo-500 tracking-wider rounded-t">
-        <i class="fas fa-close" style="color:red;margin:5px;font-size:22px;" ></i> Delivered <p id="deliver">11 Jun 2021</p>
+      <a class="sm:px-6 py-3 w-1/2 sm:w-auto justify-center sm:justify-start border-b-2 title-font font-medium   items-center leading-none  text-indigo-500 tracking-wider rounded-t" id="bgdeliver">
+        <i class="fas fa-check" style="color:green;margin:5px;font-size:22px;" id="del"></i> Delivered <p id="deliver"></p>
       </a>
-      <a class="sm:px-6 py-3 w-1/2 sm:w-auto justify-center sm:justify-start border-b-2 title-font font-medium   items-center leading-none  text-indigo-500 tracking-wider rounded-t">
-        <i class="fas fa-close" style="color:red;margin:5px;font-size:22px;" ></i> Invoiced <p id="invoice">11 Jun 2021</p>
+      <a class="sm:px-6 py-3 w-1/2 sm:w-auto justify-center sm:justify-start border-b-2 title-font font-medium   items-center leading-none  text-indigo-500 tracking-wider rounded-t" id="bginvoice">
+        <i class="fas fa-close" style="color:red;margin:5px;font-size:22px;" id="inv"></i> Invoiced <p id="invoice"><?php //echo $res['INVOICEDATE']->format('d-m-Y')?></p>
       </a>
     </div>
   </div>
@@ -79,59 +107,24 @@ if( isset($_GET['salesid'])){
           <tr class="table-bordered text-black text-center">
               <th scope="col">SO Number</th>
               <th scope="col">Packing Slip ID</th>
-              <th scope="col">Scheduled Date</th>
-              <th scope="col">Ship Date</th>
-              <th scope="col">Submitted Date</th>
               <th scope="col">Item Code</th>
               <th scope="col">Item Name</th>
               <th scope="col">UoM</th>
               <th scope="col">Recieving Quantity</th>
               <th scope="col">Invoice ID</th>
-              <th scope="col">Invoice Date</th>
 
           </tr>
         </thead>
         <tbody>
-        <?php
-
-              $query = "select ST.SALESID,PSJ.PACKINGSLIPID,PSJ.DELIVERYDATE AS SHIPDATE,
-              ST.SHIPPINGDATEREQUESTED AS SCHEDULEDATE,
-
-              ST.CREATEDDATETIME AS SUBMITEDDATE,CPST.ITEMID AS ITEMCODE, 
-              CPST.NAME ITEMNAME,CPST.SALESUNIT AS UoM
-              
-              ,CPST.QTY RCVNGQTY,CJR.INVOICEID,CJR.INVOICEDATE
-              
-              FROM SALESTABLE ST
-              
-              INNER JOIN CUSTPACKINGSLIPJOUR PSJ ON ST.SALESID = PSJ.SALESID
-              
-              INNER JOIN CUSTPACKINGSLIPTRANS CPST ON PSJ.PACKINGSLIPID = CPST.PACKINGSLIPID
-
-              INNER JOIN CUSTINVOICEJOUR AS CJR ON CJR.SALESID = ST.SALESID
-
-
-              WHERE ST.SALESID = '".$saleid."' ";
-
-              $stmt = sqlsrv_query($conn, $query, array(), array("Scrollable"=>'static')) or DIE(sqlsrv_errors());
-             
-            //   $res = sqlsrv_fetch_array($stmt);
-                
-                while($res = sqlsrv_fetch_array($stmt)){
-          ?>
-
+        
               <tr class="text-black text-center">
               <td><?php echo $res['SALESID'];?></td>
               <td><?php echo $res['PACKINGSLIPID'];?></td>
-              <td><?php echo $res['SCHEDULEDATE']->format('Y-m-d')?></td>
-              <td><?php echo $res['SHIPDATE']->format('Y-m-d');?></td>
-              <td><?php echo $res['SUBMITEDDATE']->format('Y-m-d')?></td>
               <td><?php echo $res['ITEMCODE'];?></td>
               <td><?php echo $res['ITEMNAME'];?></td>
               <td><?php echo $res['UoM'];?></td>
               <td><?php echo number_format($res['RCVNGQTY'],2) ;?></td>
               <td><?php echo $res['INVOICEID'];?></td>
-              <td><?php echo $res['INVOICEDATE']->format('Y-m-d')?></td>
 
           </tr>
           <?php 
@@ -149,6 +142,68 @@ if( isset($_GET['salesid'])){
             dom: ''
           })
         })
+
+        //date work
+
+        let submit = document.getElementById('submit').innerText;
+        let schedule = document.getElementById('schedule').innerText;
+        let ship = document.getElementById('ship').innerText;
+        let invoice = document.getElementById('invoice').innerText;
+        let deliver = document.getElementById('deliver').innerText;
+
+
+        if(submit){
+          document.getElementById('sub').className = "fas fa-check";
+          document.getElementById('sub').style.color = "green";
+        }else{
+          document.getElementById('sub').className = "fas fa-close";
+          document.getElementById('sub').style.color = "red";
+        }
+        if(schedule){
+          document.getElementById('sch').className = "fas fa-check";
+          document.getElementById('sch').style.color = "green";
+        }else{
+          document.getElementById('sch').className = "fas fa-close";
+          document.getElementById('sch').style.color = "red";
+        }
+        if(ship){
+          document.getElementById('shi').className = "fas fa-check";
+          document.getElementById('shi').style.color = "green";
+        }else{
+          document.getElementById('shi').className = "fas fa-close";
+          document.getElementById('shi').style.color = "red";
+        }
+        if(deliver){
+          document.getElementById('del').className = "fas fa-check";
+          document.getElementById('del').style.color = "green";
+        }else{
+          document.getElementById('del').className = "fas fa-close";
+          document.getElementById('del').style.color = "red";
+        }
+        if(invoice){
+          document.getElementById('inv').className = "fas fa-check";
+          document.getElementById('inv').style.color = "green";
+        }else{
+          document.getElementById('inv').className = "fas fa-close";
+          document.getElementById('inv').style.color = "red";
+        }
+       
+        
+        // backgroundcolor 
+        if(invoice){
+          $('#bginvoice').addClass('bg-gray-100 border-indigo-500');
+        }else if(deliver){
+          $('#bgdeliver').addClass('bg-gray-100 border-indigo-500'); 
+        }else if(ship){
+          $('#bgship').addClass('bg-gray-100 border-indigo-500');
+        }else if(schedule){
+          $('#bgschedule').addClass('bg-gray-100 border-indigo-500');
+        }else if(submit){
+          $('#bgsubmit').addClass('bg-gray-100 border-indigo-500');
+        }
+
+        
+        
         
 
 
